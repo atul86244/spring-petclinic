32587768b82ff7c103d9d3984884bc20541754fe
Atul <atul@chef.io>
2016-02-21 17:00:43 +0530

    fix tomcat errors and smoke tests

--- .delivery/build-cookbook/files/default/smoke_test.rb
+++ .delivery/build-cookbook/files/default/smoke_test.rb
@@ -2,10 +2,10 @@ control "java-app-test" do
 	impact 1.0
 	title "Smoke test for java app"
 	desc "Tests to make sure the java app is accessible and working"
-	describe port(9966) do
+	describe port(8080) do
 		it { should be_listening}
 	end
-	describe command('curl http://localhost:9966/petclinic') do
+	describe command('curl http://localhost:8080/petclinic') do
 		its('exit_status') { should eq 0 }
 	end
 end
