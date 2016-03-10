80c3f0f8e47c921b66677b2076e7db6edff7245e
Atul <atul@chef.io>
2016-03-10 23:09:21 +0530

    added deploy cookbook to the code base

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -6,8 +6,13 @@
 
 log "Executing Publish Phase"
 
+include_recipe "delivery-truck::publish"
+
+software_version = Time.now.strftime('%F_%H%M')
+build_name = "#{node['delivery']['change']['project']}-#{software_version}"
+
 # Remove existing war file, the mvn package command should recreate it
-file "#{node['delivery']['workspace']['repo']}/target/petclinic.war" do
+file "#{node['delivery']['workspace']['repo']}/target/#{node['delivery']['change']['project']}*.war" do
   action :delete
 end
 
@@ -20,13 +25,21 @@ execute 'mvn package' do
   action :run
 end
 
+# Rename war file 
+execute 'rename war' do
+  action :run
+  cwd "#{node['delivery']['workspace']['repo']}/target"
+  command "mv petclinic.war #{build_name}.war"
+  only_if {::File.exists?("#{node['delivery']['workspace']['repo']}/target/petclinic.war") }
+end
+
 # Upload Package to S3
 require 'rubygems'
 #require 'aws-sdk'
 require 'aws/s3'
 
 bucket_name = node['build-cookbook']['s3']['bucket_name']
-file_name = "#{node['delivery']['workspace']['repo']}/target/petclinic.war"
+file_name = "#{node['delivery']['workspace']['repo']}/target/#{build_name}.war"
 key = File.basename(file_name)
 
 log "Uploading artifact to S3"
@@ -44,4 +57,54 @@ with_server_config do
     s3.buckets[bucket_name].objects[key].write(:file => file_name)    
    end
  end
+end
+
+checksum = ''
+ruby_block 'get checksum' do
+  block do
+    checksum = `shasum -a 256 #{file_name}.war`.split[0]
+  end
+end
+
+ruby_block 'upload data bag' do
+  block do
+    with_server_config do
+      dbag = Chef::DataBag.new
+      dbag.name(node['delivery']['change']['project'])
+      dbag.save
+      dbag_data = {
+        'id' => "app_details",
+        'version' => software_version,
+        'artifact_location' => "https://s3-us-west-2.amazonaws.com/atul-java-app/#{build_name}.war",
+        'artifact_checksum' => checksum,
+        'artifact_type' => 'http',
+        'delivery_data' => node['delivery']
+      }
+      dbag_item = Chef::DataBagItem.new
+      dbag_item.data_bag(dbag.name)
+      dbag_item.raw_data = dbag_data
+      dbag_item.save
+    end
+  end
+end
+
+ruby_block 'set the version in the env' do
+  block do
+    with_server_config do
+      begin
+        to_env = Chef::Environment.load(get_acceptance_environment)
+      rescue Net::HTTPServerException => http_e
+        raise http_e unless http_e.response.code == "404"
+        Chef::Log.info("Creating Environment #{get_acceptance_environment}")
+        to_env = Chef::Environment.new()
+        to_env.name(get_acceptance_environment)
+        to_env.create
+      end
+
+      to_env.override_attributes['applications'] ||= {}
+      to_env.override_attributes['applications'][node['delivery']['change']['project']] = software_version
+      to_env.save
+      ::Chef::Log.info("Set #{node['delivery']['change']['project']}'s version to #{software_version} in #{node['delivery']['change']['project']}.")
+    end
+  end
 end
