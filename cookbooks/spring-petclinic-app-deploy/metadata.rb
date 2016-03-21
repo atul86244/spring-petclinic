74ba1c3abfab631e930318f026d7d29157cbcbea
Atul <atul@chef.io>
2016-03-21 11:16:59 +0530

    added inspec tests

--- cookbooks/spring-petclinic-app-deploy/metadata.rb
+++ cookbooks/spring-petclinic-app-deploy/metadata.rb
@@ -8,4 +8,3 @@ version '0.1.1'
 
 depends 'tomcat', '= 1.0.1'
 depends 'java', '~> 1.39.0'
-depends 'application_java'
--- cookbooks/spring-petclinic-app-deploy/recipes/default.rb
+++ cookbooks/spring-petclinic-app-deploy/recipes/default.rb
@@ -8,62 +8,49 @@ app_data = data_bag_item("spring_petclinic_new","app_details")
 
 node.default['java']['jdk_version'] = "7"
 
-# Install curl - required by kitchen for testing
-package 'curl' do
-  action :install
-end
-
 # Install Java
 include_recipe 'java'
-
-#node.default['tomcat']['base_version'] = 7
 
 # Install Tomcat
 include_recipe 'tomcat'
 
+# Install curl - Required by kitchen
+execute 'apt-get update' do
+  command 'apt-get update'
+  action :run
+end
+
+package 'curl' do
+  action :install
+end
+
 tomcat_webapps_dir = node['tomcat']['webapp_dir']
 tomcat_service_name = node['tomcat']['base_instance']
 
-service 'tomcat' do
+# Stop Tomcat
+service tomcat_service_name do
+  action :stop
+end
+
+# Clean webapps folder
+directory "#{tomcat_webapps_dir}/petclinic" do
+  action :delete
+  recursive true
+end
+
+# Download war to tomcat webapps
+remote_file "#{tomcat_webapps_dir}/petclinic.war.zip" do
+  owner 'root'
+  group 'root'
+  mode '0775'
+  source app_data['artifact_location']
+  notifies :run, 'execute[rename_petclinic.war.zip]'
+end
+
+execute 'rename_petclinic.war.zip' do
+  command 'mv -f petclinic.war.zip petclinic.war'
+  cwd tomcat_webapps_dir
   action :nothing
-  service_name tomcat_service_name
+  notifies :restart, "service[#{tomcat_service_name}]"
 end
-
-# Deploy app
-application 'petclinic' do
-  path '/var/www/petclinic'
-  repository app_data['artifact_location']
-  revision app_data['version']
-  scm_provider Chef::Provider::RemoteFile::Deploy
-
-  java_webapp
-  #tomcat
-end
-
-# # Stop Tomcat
-# service tomcat_service_name do
-#   action :stop
-# end
-
-# # Clean webapps folder
-# directory "#{tomcat_webapps_dir}/petclinic" do
-#   action :delete
-#   recursive true
-# end
-
-# # Download war to tomcat webapps
-# remote_file "#{tomcat_webapps_dir}/petclinic.war.zip" do
-#   owner 'root'
-#   group 'root'
-#   mode '0775'
-#   source app_data['artifact_location']
-#   notifies :run, 'execute[rename_petclinic.war.zip]'
-# end
-
-# execute 'rename_petclinic.war.zip' do
-#   command 'mv -f petclinic.war.zip petclinic.war'
-#   cwd tomcat_webapps_dir
-#   action :nothing
-#   notifies :restart, "service[#{tomcat_service_name}]"
-# end
 
