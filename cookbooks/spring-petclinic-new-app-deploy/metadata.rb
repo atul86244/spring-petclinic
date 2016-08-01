485b08d65dbda2095c9831d4e63514062e06a24b
Chef Sales Engineering <saleseng@chef.io>
2016-08-01 11:28:30 +0000

    updateKitchen

--- cookbooks/spring-petclinic-new-app-deploy/metadata.rb
+++ cookbooks/spring-petclinic-new-app-deploy/metadata.rb
@@ -6,5 +6,5 @@ description 'Deploys a java app'
 long_description 'A simple cookbook to deploy a java app'
 version '0.1.5'
 
-depends 'tomcat', '= 1.0.1'
+depends 'tomcat', '~> 2.3.1'
 depends 'java', '~> 1.39.0'
