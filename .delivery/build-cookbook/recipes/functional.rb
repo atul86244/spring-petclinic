12e523fcd904a102aa93d60798e23ae5aeb5c6b7
Chef Sales Engineering <saleseng@chef.io>
2016-08-03 12:46:04 +0000

    Testing Security Phase

--- .delivery/build-cookbook/recipes/functional.rb
+++ .delivery/build-cookbook/recipes/functional.rb
@@ -4,15 +4,13 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-search_query = "(#{search_terms.join(' OR ')}) " \
-                 "AND chef_environment:#{delivery_environment} " \
-                 "AND #{deployment_search_query}"
+search_query = "recipes:#{node['delivery']['change']['project']}* AND chef_environment:#{delivery_environment} " 
 
-  my_nodes = delivery_chef_server_search(:node, search_query)
+my_nodes = delivery_chef_server_search(:node, search_query)
+ 
+my_nodes.map!(&:name)
 
-  my_nodes.map!(&:name)
-
-  delivery_push_job "deploy_#{node['delivery']['change']['project']}" do
-    command 'chef-client -r "role[audit_tomcat]"'
-    nodes my_nodes
-  end
+delivery_push_job "deploy_#{node['delivery']['change']['project']}" do
+  command 'chef-client -r "role[audit_tomcat]"'
+  nodes my_nodes
+end
