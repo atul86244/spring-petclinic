9bf2effef734b91e732fd2962029abb59c4fddab
chef <chef@chef.io>
2016-08-15 10:38:36 +0100

    Updated build cookbook

--- .delivery/build-cookbook/attributes/default.rb
+++ .delivery/build-cookbook/attributes/default.rb
@@ -1,3 +1,4 @@
+default['build-cookbook']['pmd']['path'] = "/opt/pmd"
 #"http://liquidtelecom.dl.sourceforge.net/project/pmd/pmd/5.1.0/pmd-bin-5.1.0.zip"
 default['build-cookbook']['s3']['bucket_name'] = "emea-techcft"
 default['build-cookbook']['ssh_user'] = "ec2-user"
